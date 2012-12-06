371923a429da0b87bb1292f5eebfad5cc40a0014
David Eichmann <EichmannD@gmail.com>
2012-12-06 11:22:39 +0000

    Clean up Workarounds make process

--- src/BB/Types.hs
+++ src/BB/Types.hs
@@ -1,15 +1,15 @@
 module BB.Types
 where
 
+import Graphics.Ogre.Types
+import OIS.Types
+
 import Foreign
 import Foreign.C.String
 import Foreign.C.Types
 
-import Graphics.Ogre.Types
-import OIS.Types
-
 type CBool = CChar -- correct?
 
-type OIS__Mouse = Mouse-- nullary data type
-type Ogre__RenderWindow = RenderWindow -- nullary data type
+type OIS__Mouse  = Mouse
+type Ogre__RenderWindow  = RenderWindow
 
